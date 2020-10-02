158d275b67fa09c050ffd2558e58a7054c070537
Ellan Esenaliev <ellan.esenaliev@axelkg.com>
2020-10-02 19:16:02 +0600

    Add Move, Notice

---
+++ Test/Entities/Move.swift
@@ -0,0 +1,15 @@
+//
+//  Move.swift
+//  Test
+//
+//  Created by Ellan Esenaliev on 10/2/20.
+//
+
+import Foundation
+
+class Move {
+    var fromPlace: String?
+    var toPlace: String?
+    var estimateTime: TimeInterval?
+}
+
---
+++ Test/Entities/Notice.swift
@@ -0,0 +1,13 @@
+//
+//  Notice.swift
+//  Test
+//
+//  Created by Ellan Esenaliev on 10/2/20.
+//
+
+import Foundation
+
+struct Notice {
+    var flightDate: Date?
+    var gate: String?
+}
