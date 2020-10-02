9ffaad7c35d71ab008d7c9e8872a2286b71f0998
Ellan Esenaliev <ellan.esenaliev@axelkg.com>
2020-10-02 19:13:13 +0600

    Init project

---
+++ Test/Entities/Event.swift
@@ -0,0 +1,14 @@
+//
+//  Event.swift
+//  Test
+//
+//  Created by Ellan Esenaliev on 10/2/20.
+//
+
+import Foundation
+
+class Event {
+    var startTime: Date?
+    var endTime: Date?
+    var name: String!
+}
